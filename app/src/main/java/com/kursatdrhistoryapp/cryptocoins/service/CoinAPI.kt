2dbe881081868c585f93f8567d6748393c4412c5
DrHistory <kursatkumsuz@gmail.com>
2022-08-10 11:34:57 +0300

    change Call with RxJava Single

--- app/src/main/java/com/kursatdrhistoryapp/cryptocoins/service/CoinAPI.kt
+++ app/src/main/java/com/kursatdrhistoryapp/cryptocoins/service/CoinAPI.kt
@@ -1,16 +1,14 @@
 package com.kursatdrhistoryapp.cryptocoins.service
 
 import com.kursatdrhistoryapp.cryptocoins.model.CoinModel
-import retrofit2.Call
+import io.reactivex.Single
 import retrofit2.http.GET
 
 interface CoinAPI {
 
-
     //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json
-    //GET
     @GET("atilsamancioglu/K21-JSONDataSet/master/crypto.json")
-    fun getData(): Call<List<CoinModel>>
+    fun getData(): Single<List<CoinModel>>
 
 
 }
